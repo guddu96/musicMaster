203a9f0ca024430416e435a1a8c4c9239a264cd0
guddu96 <shuvankar.guddu@gmail.com>
2020-08-16 19:06:22 +0530

    iniatl commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './Components/App';
+// import { Router, Switch, Route } from 'react-router-dom';
+// import createBrowserHistory from 'history/createBrowserHistory';
+// import Jokes from './Components/Jokes';
+import './index.css';
+
+ReactDOM.render(<App/>, document.getElementById('root'));
+
+
